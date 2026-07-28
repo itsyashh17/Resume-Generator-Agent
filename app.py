506139aadaab8ec314a5a154c8d8a5ef9b3851b0
itsyashh17 <yashhestic@gmail.com>
2026-07-28 15:53:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ final_prompt = prompt + prompt_reader()
 
 FILE = st.sidebar.file_uploader(
      "Choose an image file",
-  type=["jpg","jpeg","png","webp]
+  type=["jpg","jpeg","png","webp"]
   )
 
 if FILE is not None:
